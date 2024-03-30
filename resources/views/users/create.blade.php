754b117ac13fff56b8c27cc762121bdb6a2dac04
sergio castillo <sergiocastillorens@gmail.com>
2024-03-30 18:08:01 +0100

    estilos para crear usuarios (aplicar a los demás)

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,34 +1,53 @@
 @extends('layout.styles')
+@extends('layout.main')
+@section('content')
 
-<div>
-    <form action="{{route('users.store')}}" method="POST">
-        <div class="form-group p-3">
+<div style="display:flex; justify-content:center; height:100%; align-items:center;">
+    <div>
+        <form action="{{route('users.store')}}" method="POST">
+            <p><a href="{{route('users.index')}}">Volver (cambiar por flecha)</a></p>
+            <h1>Crear usuario</h1>
             <!-- protection against injections -->
             @csrf
-            <label for="name">Nombre</label>
-            <input type="text" name="name" id="name">
+            <div class="row">
+                <div class="col">
+                    <label for="name">Nombre</label><br>
+                    <input type="text" name="name" id="name">
+                </div>
 
-            <label for="surname">Apellidos</label>
-            <input type="text" name="surname" id="surname">
-        </div>
-        <div class="form-group p-3">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email">
+                <div class="col">
+                    <label for="surname">Apellidos</label>
+                    <input type="text" name="surname" id="surname">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="email">Email</label><br>
+                    <input type="text" name="email" id="email">
+                </div>
+                <div class="col">
 
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div class="form-group p-3">
-            <label for="dni">DNI</label>
-            <input type="text" name="dni" id="dni">
+                    <label for="password">Contraseña</label><br>
+                    <input type="password" name="password" id="password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="dni">DNI</label><br>
+                    <input type="text" name="dni" id="dni">
+                </div>
 
-            <label for="type">Tipo</label>
-            <select name="type" id="type">
-                <option selected disabled>--selecciona un tipo</option>
-                <option value="0">Usuario</option>
-                <option value="1">Admin</option>
-            </select>
-        </div>
-        <button class="btn" type="submit">Crear usuario</button>
-    </form>
+                <div class="col">
+                    <label for="type">Tipo</label><br>
+                    <select name="type" id="type">
+                        <option selected disabled>--selecciona un tipo</option>
+                        <option value="0">Usuario</option>
+                        <option value="1">Admin</option>
+                    </select>
+                </div>
+            </div>
+            <button class="btn" type="submit">Crear usuario</button>
+        </form>
+    </div>
 </div>
+@endsection
