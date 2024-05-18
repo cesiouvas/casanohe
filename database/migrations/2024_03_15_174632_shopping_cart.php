b8557accd52fe80da48f4d0fe1c7e515184bbddb
sergio castillo <sergiocastillorens@gmail.com>
2024-05-18 18:08:05 +0200

    fux migration

--- database/migrations/2024_03_15_174632_shopping_cart.php
+++ database/migrations/2024_03_15_174632_shopping_cart.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shoppingCart', function (Blueprint $table) {
+        Schema::create('shoppingcart', function (Blueprint $table) {
             $table->id();
             $table->integer('line_quantity');
             $table->decimal('line_price', 15, 2);
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('shoppingCart');
+        Schema::dropIfExists('shoppingcart');
     }
 };
